d25c36ce2214a69f2473fd61efb1b501647ac8a9
saurabh2836 <saurabh2836@gmail.com>
2024-08-03 21:00:04 +0530

    push the code

--- code/contracts/MyContract.sol
+++ code/contracts/MyContract.sol
@@ -6,7 +6,24 @@ pragma solidity ^0.8.0;
 
 contract MyContract {
               //State Variables  
-            uint myUint = 1;
+            int  public myUint = 1;
+            uint256 public myUint256  =1;
+            uint public myUnit8 =1;
+
+            string public myString = " Hello, world !";
+            bytes32 public myBytes32 ="Hello , world !";
+
+          //  address public myAddress = 0xC02AAsddsASDSDSDSDSDSDSD;
+
+            struct MyStruct {
+
+                uint256 myUint256;
+                string myString;
+            }
+
+
+
+            MyStruct public mystruct = MyStruct(1,"Hello, world");
 
             function getValue() public pure returns(uint)  {
 
